a60884436f9392361f044d2f371ed12d6dcb926f
superdev93919 <jinge93919.dev@gmail.com>
2023-04-09 11:09:54 +0200

    feat(typing): add color and unit types
    Add `Color` type to `src/lib/utils/typing/color.ts` and `Rem`, `Pixel`, `Percent`, `ViewportWidth`, and `ViewportHeight` types to `src/lib/utils/typing/unit.ts`.

---
+++ src/lib/utils/typing/unit.ts
@@ -0,0 +1,5 @@
+export type Rem = `${number}rem`;
+export type Pixel = `${number}px`;
+export type Percent = `${number}%`;
+export type ViewportWidth = `${number}vw`;
+export type ViewportHeight = `${number}vh`;
