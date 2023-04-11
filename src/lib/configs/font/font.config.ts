7485554b3b1951877ba96c39cb148813c395b104
superdev93919 <jinge93919.dev@gmail.com>
2023-04-11 16:00:31 +0200

    feat(font.config.ts): add Inter and Roboto Mono font configurations feat(index.ts): export font configurations from font.config.ts

---
+++ src/lib/configs/font/font.config.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line camelcase
+import { Inter, Roboto_Mono } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter"
+});
+
+export const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono"
+});
